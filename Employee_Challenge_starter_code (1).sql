a078d73df925820e9c2a2bb9cc3cb2a612169f11
aqua2504 <aakriti2504@gmail.com>
2021-12-07 13:32:23 -0800

    initial upload

---
+++ Employee_Challenge_starter_code (1).sql
@@ -0,0 +1,9 @@
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (______) _____,
+emp_no,
+first_name,
+______
+
+INTO nameyourtable
+FROM _______
+ORDER BY _____, _____ DESC;
---
+++ Queries/Employee_Database_Challenge.sql
@@ -0,0 +1,2 @@
+select emp_no, first_name, last_name
+from Employees;
